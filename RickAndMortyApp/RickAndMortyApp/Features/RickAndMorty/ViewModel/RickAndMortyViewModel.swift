c2eea84f86d952d310d480dd7797cb82e44be964
Okariuss <orkunokann@gmail.com>
2023-07-02 00:05:15 +0300

    Table View Created

--- RickAndMortyApp/RickAndMortyApp/Features/RickAndMorty/ViewModel/RickAndMortyViewModel.swift
+++ RickAndMortyApp/RickAndMortyApp/Features/RickAndMorty/ViewModel/RickAndMortyViewModel.swift
@@ -14,10 +14,20 @@ protocol IRickAndMortyViewModel {
     
     var rickAndMortyCharacters: [Result] {get set}
     var rickAndMortyService: IRickAndMortyService {get}
+    
+    var rickAndMortyOutput: RickAndMortyOutput? { get }
+    
+    func setDelegate(output: RickAndMortyOutput)
 }
 
 
-class RickAndMortyViewModel: IRickAndMortyViewModel {
+final class RickAndMortyViewModel: IRickAndMortyViewModel {
+    var rickAndMortyOutput: RickAndMortyOutput?
+    
+    func setDelegate(output: RickAndMortyOutput) {
+        rickAndMortyOutput = output
+    }
+    
     
     var rickAndMortyCharacters: [Result] = []
     private var isLoading = false
@@ -28,14 +38,17 @@ class RickAndMortyViewModel: IRickAndMortyViewModel {
     }
     
     func fetchItems() {
+        changeLoading()
         rickAndMortyService.fetchAllDatas { [weak self] (responce) in
             self?.changeLoading()
             self?.rickAndMortyCharacters = responce ?? []
+            self?.rickAndMortyOutput?.saveDatas(values: self?.rickAndMortyCharacters ?? [])
         }
     }
     
     func changeLoading() {
-        
+        isLoading = !isLoading
+        rickAndMortyOutput?.changeLoading(isLoad: isLoading)
     }
     
     
