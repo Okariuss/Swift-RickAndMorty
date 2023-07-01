7882cf1e96acee2ed2e8a8cf803ac6aefe1d833a
Okariuss <orkunokann@gmail.com>
2023-07-01 21:20:05 +0300

    Basic View Created

--- RickAndMortyApp/RickAndMortyApp/Features/RickAndMorty/View/RickAndMortyViewController.swift
+++ RickAndMortyApp/RickAndMortyApp/Features/RickAndMorty/View/RickAndMortyViewController.swift
@@ -6,24 +6,67 @@
 //
 
 import UIKit
+import SnapKit
 
 final class RickAndMortyViewController: UIViewController {
+    
+    private let labelTitle: UILabel = UILabel()
+    private let box: UIView = UIView()
+    private let indicator: UIActivityIndicatorView = UIActivityIndicatorView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configure()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configure() {
+        view.addSubview(labelTitle)
+        view.addSubview(box)
+        view.addSubview(indicator)
+        
+        drawDesign()
+        
+        makeLabelTitle()
+        
+        makeIndicator()
+        
+        makeBox()
     }
-    */
-
+    
+    private func drawDesign() {
+        DispatchQueue.main.async {
+            self.view.backgroundColor = .white
+            self.box.backgroundColor = .red
+            
+            self.labelTitle.text = "oO10"
+            self.indicator.color = .red
+        }
+        indicator.startAnimating()
+    }
+}
+extension RickAndMortyViewController {
+    private func makeBox() {
+        box.snp.makeConstraints { make in
+            make.top.equalTo(labelTitle.snp.bottom).offset(10)
+            make.bottom.equalToSuperview()
+            make.left.right.equalTo(labelTitle)
+        }
+    }
+    
+    private func makeIndicator() {
+        indicator.snp.makeConstraints { make in
+            make.height.equalTo(labelTitle)
+            make.right.equalTo(labelTitle).offset(-5)
+            make.top.equalTo(labelTitle)
+        }
+    }
+    
+    private func makeLabelTitle() {
+        labelTitle.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
+            make.left.equalTo(view).offset(10)
+            make.right.equalTo(view).offset(-10)
+            make.height.greaterThanOrEqualTo(10)
+        }
+    }
 }
